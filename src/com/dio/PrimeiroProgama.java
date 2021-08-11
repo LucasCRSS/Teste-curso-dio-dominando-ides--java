64e9f24bd03423101312fef162aefd9bda531364
LucasCRSS <Lucascoloriano@gmail.com>
2021-08-11 14:04:41 -0300

    first commit

---
+++ src/com/dio/PrimeiroProgama.java
@@ -0,0 +1,31 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+/**
+ * @author LUCAS
+ *
+ */
+public class PrimeiroProgama {
+	public static void main(String[] args) {
+	
+		Gato gato = new Gato();
+	    Livros livros = new Livros();
+	
+	System.out.println(gato);
+	System.out.println(livros);
+
+	
+	/*public static void main(String[] args) {
+		int a = 2;
+		int b = 5;
+		
+		System.out.println("HELLO WORLD!!" + (a+b));*/
+}
+
+}
+class Livros {
+	private String nome;
+	private String npag;
+	
+}
